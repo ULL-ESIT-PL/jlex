9f9400a8f90065108f551079361099fce0201b06
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2026-02-12 22:16:19 +0000

    adding options version help and output

--- jlex.js
+++ jlex.js
@@ -2,11 +2,29 @@
 // replace example by the name of the generated module
 const fs = require("fs");
 const { execSync } = require('child_process');
-const fileName = process.argv[2];
-console.log("Processing file:", fileName);
-execSync(`npx jison-lex ${fileName}.l`, { encoding: 'utf-8' });
-let lexerStr = fs.readFileSync(`${fileName}.js`, "utf8").toString();
+const { Command } = require('commander')
+const packageJson = require('./package.json')
+const path = require('path');
+const program = new Command();
+
+program
+  .version(packageJson.version)
+  .description('A tiny wrapper around jison-lex that allows you to use jison-lex as a standalone (flex like) processor.')
+  .option("-o <fileName>", "Output file name")
+  .usage("[options] <filename>");
+
+program.parse(process.argv);
+const options = program.opts();
+
+const fileName = program.args[0];
+console.log(fileName);
+const {dir, base, ext, name } = path.parse(fileName); // { dir, base, ext, name }
+const outputFileName = options.o || path.join(dir, `${name}.js`);
+const outputParse = path.parse(outputFileName);
+const shellCommand = `npx jison-lex ${fileName} -o ${outputFileName}`;
+execSync(shellCommand, { encoding: 'utf-8' });
+let lexerStr = fs.readFileSync(outputFileName, "utf8").toString();
 //console.log("Processing file:", lexerStr);
-let lexerModule = lexerStr.replace(new RegExp(`var ${fileName} =`, 'g'), `\nmodule.exports =`);
-console.log("Writing file:", `${fileName}.js`);
-fs.writeFileSync(`${fileName}.js`, lexerModule);
+let lexerModule = lexerStr.replace(new RegExp(`var ${outputParse.name} =`), `\nmodule.exports =`);
+console.log("Writing file:", outputFileName);
+fs.writeFileSync(outputFileName, lexerModule);
